68cd80d7f97287bf577f86adab5546c03b96e48a
Henry Schaumburger <per4mnce@gmail.com>
2015-12-31 14:46:41 -0500

    Checkpoint 3 Routing and States

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,30 @@
-function config($stateProvider, $locationProvider) {
-    $locationProvider
-        .html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-    
-    $stateProvider
-    .state('landing', {
-        url: '/',
-        templateUrl: '/templates/landing.html'
-    })
-    .state('album', {
-        url: '/album',
-        templateUrl: '/templates/album.html'
-    });
-}
-
 (function () {
-    function config($stateProvider, $locationProvider) {}
+    console.log("app.js start");
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
+    }
 
     angular
         .module('blocJams', ['ui.router'])
         .config(config);
+    
+    console.log("app.js end");
 })();
