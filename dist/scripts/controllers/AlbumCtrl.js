464da1a01a45ed402fe014901062afb055dfd3d2
Henry Schaumburger <per4mnce@gmail.com>
2016-01-07 15:15:03 -0500

    Checkpoint 07 Services 2: Completed

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,10 @@
 (function() {
-   function AlbumCtrl(Fixtures) {
-       console.log("AlbumCtrl")
-       this.albumData = Fixtures.getAlbum();
+    function AlbumCtrl(Fixtures, SongPlayer) {
+        this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
    }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
